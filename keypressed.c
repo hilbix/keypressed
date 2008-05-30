01e97f535144beed4d6f5e7cf2723486dc3dc7ab
Valentin Hilbig <webmaster@scylla-charybdis.com>
2008-05-30 17:03:51 +0000

    Version now in the binary, too

--- keypressed.c
+++ keypressed.c
@@ -6,9 +6,19 @@
  * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
  *
  * $Log$
+ * Revision 1.2  2008-05-30 17:03:51  tino
+ * Version now in the binary, too
+ *
  * Revision 1.1  2008-05-30 16:59:00  tino
  * First try
- *
+ */
+
+/* Encode the version in the binary
+ */
+#include "keypressed_version.h"
+const char version_string[]="VERSION " KEYPRESSED_VERSION;
+
+/* Create the main program
  */
 #define TINO_TEST_MAIN
 #include "tino_keypressed.h"
