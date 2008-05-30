b15657f64078ca1102c4c946f091a42775c5e509
Valentin Hilbig <webmaster@scylla-charybdis.com>
2008-05-30 16:59:00 +0000

    First try

---
+++ keypressed.c
@@ -0,0 +1,14 @@
+/* $Header$
+ *
+ * See tino_keypressed.h
+ *
+ * This Works is placed under the terms of the Copyright Less License,
+ * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
+ *
+ * $Log$
+ * Revision 1.1  2008-05-30 16:59:00  tino
+ * First try
+ *
+ */
+#define TINO_TEST_MAIN
+#include "tino_keypressed.h"
